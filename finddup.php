247d08258a51e107c2e025df072ed2213e328040
jsavoie <julien.savoie@usainteanne.ca>
2015-02-01 18:16:19 -0400

    Added php version

---
+++ finddup.php
@@ -0,0 +1,49 @@
+<?
+$filelist = array();
+$lastfilesize = -1;
+$lastfilename = '';
+
+function joinpath() {
+    $paths = array();
+
+    foreach (func_get_args() as $arg)
+        if ($arg !== '')
+		$paths[] = $arg;
+
+    return preg_replace('#/+#','/',join('/', $paths));
+}
+
+function processdir($dir, &$filelist)
+{
+	if ($directoryhandler = opendir($dir))
+		while ($file = readdir($directoryhandler))
+			if (($file != '.') and ($file != '..'))
+			{
+				$fullpath = joinpath($dir, $file);
+				if (is_file($fullpath) AND (!is_link($fullpath)))
+					$filelist[$fullpath] = filesize($fullpath);
+				elseif (is_dir($fullpath))
+					// further down the rabbit hole you go
+					processdir($fullpath, $filelist);
+			}
+	closedir($directoryhandler);
+}
+
+foreach (array_slice($argv,1) as $directory)
+{
+	if (is_dir($directory))
+		processdir($directory, $filelist);
+}
+
+asort($filelist, SORT_NUMERIC);
+foreach ($filelist as $filename => $filesize)
+{
+	// being the same size is a necessary, but not sufficient condition
+	if ($filesize == $lastfilesize)
+		if (sha1_file($filename) == sha1_file($lastfilename))
+			echo $filename . " and " . $lastfilename . " are duplicates\n";
+
+	$lastfilename = $filename;
+	$lastfilesize = $filesize;
+}
+?>
